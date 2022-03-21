3105cb678d463f5eb18745e2085d41ce598b3387
jslipak <seriedgigital@gmail.com>
2022-03-21 18:19:57 -0300

    hola add 21 march 2022

--- src/app.js
+++ src/app.js
@@ -2,17 +2,44 @@ import express from 'express';
 import { readFileSync } from 'fs';
 const app = express();
 const puerto = 8080;
-let visitas = { visitas: { items: 0, item: 0 } };
-const data = JSON.parse(readFileSync('./productos.json', 'utf8'));
 
-app.get('/items', (_req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const data = JSON.parse(readFileSync('./src/productos.json', 'utf8'));
+let visitas = { visitas: { items: 0, item: 0 } };
+
+app.get('/api/productos/listar', (_req, res) => {
   res.json({ items: data, cantidad: data.length });
   visitas.visitas.items = visitas.visitas.items + 1;
 });
-app.get('/item-random', (_req, res) => {
+
+app.get('/api/productos/listar/:id', (req, res) => {
+  console.log(req.params.id);
+  let obj = data.find((x) => x.id == req.params.id);
+  res.json({ item: obj });
+});
+
+app.post('/api/productos/listar', async (req, res) => {
+  const objectArray = await import('./services/objectArray.services.js');
+  const arrayClass = new objectArray.default(data);
+  console.log(arrayClass);
+  const maxId = await arrayClass.getMaxId();
+
+  const newProduct = {
+    id: maxId + 1,
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+  };
+  data.push(newProduct);
+  res.json({ item: newProduct });
+});
+app.get('/api/productos/item-random', (_req, res) => {
   res.json(data[Math.floor(Math.random() * data.length)]);
   visitas.visitas.item = visitas.visitas.item + 1;
 });
+
 app.get('/visitas', (_req, res) => {
   res.json(visitas);
 });
