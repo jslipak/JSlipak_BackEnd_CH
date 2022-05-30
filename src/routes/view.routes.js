bee8e0052ce7d0e26216163ff4c3e1b949048827
jslipak <seriedgigital@gmail.com>
2022-05-30 15:34:02 -0300

    fix login , avoid show login when you are loded

--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const auth = require('../middleware/auth.middleware');
 
 router.get('/', (req, res) => {
-  res.render('pages/login');
+  req.session.user
+    ? res.render('pages/home', {
+        name: req.session.user,
+        msg: `ya estas logueado ${req.session.user}`,
+      })
+    : res.render('pages/login');
 });
 router.get('/signup', (req, res) => {
   res.render('pages/signup');
 });
 
 router.get('/home', auth, (req, res) => {
-  res.render('pages/home', { name: req.session.user });
+  res.render('pages/home', { name: req.session.user, msg: false });
 });
 
 module.exports = router;
